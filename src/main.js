807cdeae388730fe24fd82edc2ab41d6cd41aab5
Rafael López Dorado <rldorado86@gmail.com>
2022-03-13 12:18:58 +0100

    Remove of old Material Design Lite

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// Import Material UI
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 Vue.config.productionTip = false
 
 new Vue({
